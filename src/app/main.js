5a7f1c290042ec27d7c3ded63ee9bb2a6a5e2b21
Janne Huotari <jannehuotari@Jannes-MacBook-Pro.local>
2018-08-17 09:02:02 +0300

    very simple engine example added

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,12 @@
-import { HELLO } from './hello'
+//https://twitter.com/MaximeEuziere/status/883044404453294080
 
-document.write(HELLO)
+const canvas = document.getElementById('game-canvas')
+const ctx = canvas.getContext('2s')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const gameState = 0
+
+setInterval(e => {
+  
+},33)
